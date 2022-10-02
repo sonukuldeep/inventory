4a06df5e8c276a5355da2b1a22657558f0b3590e
sonukuldeep <sonukuldeep@outlook.com>
2022-10-02 12:10:48 +0530

    working on sidebar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
 
       <section>
         <article>
-
         </article>
       </section>
     </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,89 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faLink, faBook } from '@fortawesome/free-solid-svg-icons'
+import { faSquareCaretDown } from '@fortawesome/free-regular-svg-icons'
 
 const Navbar = () => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-dark" style={{borderBottom:'2px solid #fff'}}>
-  <div className="container-fluid">
-    <a className="navbar-brand text-white ms-4" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="ms-5 d-none d-sm-inline text-decoration-none active" style={{paddingLeft:'2rem'}} aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="ms-5 d-none d-sm-inline text-decoration-none" style={{paddingLeft:'2rem'}} href="#">Link</a>
-        </li>
-        
-        <li className="nav-item">
-          <a className="ms-5 d-none d-sm-inline text-decoration-none" style={{paddingLeft:'2rem'}} href="#">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg" style={{backgroundColor: '#e3f2fd'}}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
     </>
   )
 }

3aec7b8ea826f0cf515c7bd8bfc6d855c1408d73
sonukuldeep <sonukuldeep@outlook.com>
2022-10-03 18:30:06 +0530

    custom nav and sidebar in progress

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 A sample inventory website using React, nodejs and MySQL 
-Bootstrap and font awesome is used to help with rapid development
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,80 +6,19 @@ import { faSquareCaretDown } from '@fortawesome/free-regular-svg-icons'
 
 
 
-
 const Sidebar = () => {
     return (
         <>
-            <div id="viewport">
-                {/* Sidebar */}
-                <div id="sidebar">
-                    <header>
-                        <a href="#">My App</a>
-                    </header>
-                    <ul className="nav">
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-view-dashboard" /> Dashboard
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-link" /> Shortcuts
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-widgets" /> Overview
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-calendar" /> Events
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-info-outline" /> About
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-settings" /> Services
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="zmdi zmdi-comment-more" /> Contact
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                {/* Content */}
-                <div id="content">
-                    <nav className="navbar navbar-default">
-                        <div className="container-fluid">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li>
-                                    <a href="#">
-                                        <i className="zmdi zmdi-notifications text-danger" />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">Test User</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <div className="container-fluid">
-                        <h1>Simple Sidebar</h1>
-                        <p>
-                            Make sure to keep all page content within the
-                            <code>#content</code>.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
+        <div className="sidebar">
+            <ul className="list-head">
+                <li>Dashboard</li>
+                <li>Products</li>
+                <li>Critical</li>
+                <li>Customers</li>
+                <li>Log out</li>
+            </ul>
+        </div>
+        <p style={{marginLeft:'200px'}}>hello</p>
         </>
     )
 }

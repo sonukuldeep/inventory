6664b3f1b0f779b76705775dece1ea2253fa290e
sonukuldeep <sonukuldeep@outlook.com>
2022-10-16 13:34:14 +0530

    rework of login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,26 +2,18 @@ import React from 'react'
 import './Login.css'
 
 const Login = () => {
-    function login(e) {
-        e.preventDefault()
-        console.log(e.target.name)
+    
+    function login() {
+        
     }
 
     return (
         <>
             <div className="background"></div>
             <div className="login-container">
-                <div className="container">
-                    <div className="color-Container">
-                        <form action="#" onSubmit={() => { login() }}>
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" />
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" />
-                            <button className="btn btn-success" type="submit">Log in</button>
-                        </form>
+                    <div className='form'>
+                        <button type="submit" onClick={() => { login() }}>Log in</button>
                     </div>
-                </div>
             </div>
         </>
     )
